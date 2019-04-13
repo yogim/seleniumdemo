2628176877b9cc05b36cc1456936879a0f0e80b3
Yogesh Mhase <yogeshmhase89@gmail.com>
2019-04-13 15:37:43 -0700

    add gmailformatstring, pom

--- src/main/java/com/ymhase/seleniumdemo/testcases/GmailFormtesting.java
+++ src/main/java/com/ymhase/seleniumdemo/testcases/GmailFormtesting.java
@@ -6,6 +6,7 @@ public class GmailFormtesting {
 
 	public static void main(String[] args) {
 		SeleniumUtils.maximizeWindow();
-		
+		SeleniumUtils.waitTillVisible(30, "#accountDetailsNext > content");
+		SeleniumUtils.clickOnElement("#accountDetailsNext > content");
 	}
 }
